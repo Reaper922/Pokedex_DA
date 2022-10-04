71993c1ff4413799bb89be4281a46ac140765bbe
Dennis Ammen <dennis.ammen@outlook.com>
2022-10-04 15:33:39 +0200

    Added sprite to Pokemon object

--- pokemon.js
+++ pokemon.js
@@ -3,6 +3,7 @@ class Pokemon {
         this.id = pokemonObj.id
         this.name = pokemonObj.name;
         this.types = pokemonObj.types;
+        this.sprite = pokemonObj.sprites.other.dream_world.front_default;
     }
 }
 
