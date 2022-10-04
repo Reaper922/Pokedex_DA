f29218963ba257f98a819386d571169a4ecd7dca
Dennis Ammen <dennis.ammen@outlook.com>
2022-10-04 15:33:14 +0200

    Added Pokemon type rendering

--- templates.js
+++ templates.js
@@ -11,11 +11,25 @@ function pokemonCardTemp(pokemon) {
         <div class="pokemon-preview-card bg-green">
             <h3 class="txt-h3">${pokemon.name}</h3>
             <div class="types">
-                <span class="type txt-body">Grass</span>
-                <span class="type txt-body">Poison</span>
+                ${pokemonTypeTemp(pokemon.types)}
             </div>
             <span class="id txt-body">#${pokemon.id}</span>
             <img src="./img/pokeball.svg" alt="Pokeball Background" class="pokeball-background">
-            <img src="./img/pokemon/${pokemon.id}.svg" alt="Pokemon Picture" class="pokemon-picture">
+            <img src="${pokemon.sprite}" alt="Pokemon Picture" class="pokemon-picture">
         </div>`;
+}
+
+
+/**
+ * Creates the Pokemon type template.
+ * @param {Array} pokemonTypes Array of the types from the Pokemon object.
+ * @return HTML Pokemon types template.
+ */
+function pokemonTypeTemp(pokemonTypes) {
+    let types = '';
+
+    pokemonTypes.forEach(type => {
+        types += `<span class="type txt-body">${type.type.name}</span>`;
+    });
+    return types;
 }
